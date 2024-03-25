bced9c19df65cc10390c276af8cd387db7c10e69
Federico Gutman <fgutman@udesa.edu.ar>
2024-03-25 14:59:50 -0300

    cambios

---
+++ tp1/punto2/main2.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange
+
+def get_coordinates(file:str) -> list:
+    data = []
+    with open(file, 'r') as f:
+        for line in f:
+            x, y = line.strip().split(" ")
+            data.append((x,y))
+    return data
+
+# grafico la trayectoria del primer vehiculo
+first_vehicle = get_coordinates("tp1/punto2/mnyo_ground_truth.csv")
+x1 = np.array([float(x) for x, _ in first_vehicle])
+y1 = np.array([float(y) for _, y in first_vehicle])
+
+plt.plot(x1, y1, color='black', label='First Vehicle')
+
+# interpolo la trayectoria del segundo vehiculo usano polinomios de lagrange y grafico
+second_vehicle = get_coordinates("tp1/punto2/mnyo_mediciones2.csv")
+x2 = np.array([float(x) for x, _ in second_vehicle])
+y2 = np.array([float(y) for _, y in second_vehicle])
+
+lagrange_poly = lagrange(x2, y2)
+
+plt.plot(x1, lagrange_poly(x1), color='blue', label='Second Vehicle')
+plt.title('Trajectories of both vehicles', fontsize=20)
+plt.legend()
+plt.show()
+
+# busco la interseccion entre las trayectorias
+
+
+
+
+
+
+
