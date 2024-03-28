a0bf5708cdeff3c6deddc38c1ae712640dda2798
Federico Gutman <fgutman@udesa.edu.ar>
2024-03-28 20:18:35 -0300

    cambio chiquito

--- tp1/punto1/puntos_equiespaciados/function2.py
+++ tp1/punto1/puntos_equiespaciados/function2.py
@@ -9,8 +9,8 @@ def f(x1, x2):
     return 0.75*np.exp( (-((10*x1-2)**2)/4) - (((9*x2-2)**2)/4) ) + 0.65*np.exp( (-((9*x1+1)**2)/9) - (((10*x2+1)**2)/2) ) + 0.55*np.exp( (-((9*x1-6)**2)/4) - (((9*x2-3)**2)/4) ) - 0.01*np.exp( (-((9*x1-7)**2)/4) - (((9*x2-3)**2)/4) )
 
 # Grafico utilizando matplotlib
-x1 = np.linspace(-1, 1, 100)
-x2 = np.linspace(-1, 1, 100)
+x1 = np.linspace(-1, 1, 500)
+x2 = np.linspace(-1, 1, 500)
 X1, X2 = np.meshgrid(x1, x2)
 Z = f(X1, X2)
 
