ab149a6cb6fbc9338bc575e13927857a242998fe
Federico Gutman <fgutman@udesa.edu.ar>
2024-05-13 11:46:08 -0300

    graficos

--- tp2/punto2.py
+++ tp2/punto2.py
@@ -77,37 +77,67 @@ for i in range(2):
 # Caso 1 -> Gana la especie 1
 alpha = 0.7
 beta = 1.2
-K1, K2 = 100, 100
-
-N2_values = np.linspace(0, K2, 1000)
-N1_isocline = K1 - alpha*N2_values
+K1, K2 = 100, 70
 
 N1_values = np.linspace(0, K1, 1000)
-N2_isocline = K2 - beta*N1_values
 
-axis[0, 0].plot(N1_isocline, N2_values, label='Especie 1', color='blue')
-axis[0, 0].plot(N1_values, N2_isocline, label='Especie 2', color='green')
+S1_isocline = (N1_values - K1)/(- alpha)
+S2_isocline = K2 - beta*N1_values
+
+axis[0, 0].plot(N1_values, S1_isocline, label='Especie 1', color='blue')
+axis[0, 0].plot(N1_values, S2_isocline, label='Especie 2', color='green')
 axis[0, 0].set_ylim(0, K1/alpha + 5)
 axis[0, 0].set_xlim(0, K1 + 5)
 axis[0, 0].legend()
 axis[0, 0].set_title('Gana especie 1')
 
 # Caso 2 -> Gana la especie 2
-
 alpha = 1.5
 beta = 0.5
-K1, K2 = 100, 100
+K1, K2 = 70, 60
 
-N2 = np.linspace(0, K2, 1000)
-N1_isocline = np.array([(K1 - alpha*n2) for n2 in N2])
+N1_values = np.linspace(0, K2/beta, 1000) # preguntar si esta bien esto
 
-N1 = np.linspace(0, K1, 1000)
-N2_isocline = np.array([(K2 - beta*n1) for n1 in N1])
+S1_isocline = (N1_values - K1)/(- alpha)
+S2_isocline = K2 - beta*N1_values
 
-axis[0, 1].plot(N1_isocline, N2, label='Especie 1', color='blue')
-axis[0, 1].plot(N1, N2_isocline, label='Especie 2', color='green')
-axis[0, 1].set_ylim(0, K1/beta + 5)
-axis[0, 1].set_xlim(0, K1 + 5)
+axis[0, 1].plot(N1_values, S1_isocline, label='Especie 1', color='blue')
+axis[0, 1].plot(N1_values, S2_isocline, label='Especie 2', color='green')
+axis[0, 1].set_ylim(0, K2 + 5)
+axis[0, 1].set_xlim(0, K2/beta + 5)
 axis[0, 1].set_title('Gana especie 2')
 
+# Caso 3 -> Puede ganar cualquiera
+alpha = 1.6
+beta = 1.3
+K1, K2 = 40, 30
+
+N1_values = np.linspace(0, K1, 1000)
+
+S1_isocline = (N1_values - K1)/(- alpha)
+S2_isocline = K2 - beta*N1_values
+
+axis[1, 0].plot(N1_values, S1_isocline, label='Especie 1', color='blue')
+axis[1, 0].plot(N1_values, S2_isocline, label='Especie 2', color='green')
+axis[1, 0].set_ylim(0, K1/alpha + 5)
+axis[1, 0].set_xlim(0, K1 + 5)
+axis[1, 0].set_title('Puede ganar cualquiera')
+
+# Caso 4 -> Coexistencia
+alpha = 1.2
+beta = 0.6
+K1, K2 = 80, 60
+
+N1_values = np.linspace(0, K2/beta, 1000)
+
+S1_isocline = (N1_values - K1)/(- alpha)
+S2_isocline = K2 - beta*N1_values
+
+axis[1, 1].plot(N1_values, S1_isocline, label='Especie 1', color='blue')
+axis[1, 1].plot(N1_values, S2_isocline, label='Especie 2', color='green')
+axis[1, 1].set_ylim(0, K1/alpha + 5)
+axis[1, 1].set_xlim(0, K2/beta + 5)
+axis[1, 1].set_title('Coexistencia')
+
+plt.subplots_adjust(wspace=0.3, hspace=0.3)
 plt.show()
